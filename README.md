1f353166675afab3c8640fd3dac4127f786e4053
Gabriel Moreno <gantoreno@gmail.com>
2022-07-02 14:13:55 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Homebrew Macfetch
+
+The [Homebrew](https://brew.sh/) tap for [Macfetch](https://github.com/gantoreno/macfetch), the macOS Neofetch alternative written in C++.
---
+++ macfetch.rb
@@ -0,0 +1,12 @@
+class Macfetch < Formula
+  desc "🍎 A macOS Neofetch alternative written in C++"
+  homepage "https://github.com/gantoreno/macfetch"
+  url "https://github.com/gantoreno/macfetch/releases/download/v1.0.0/macfetch-v1.0.0.tar.gz"
+  sha256 "bd8531b143e34162d2844cc2522760aaa265c79362cdd1f328255b0791ee85d9"
+  license "MIT"
+
+  def install
+    bin.install "macfetch"
+  end
+end
+
