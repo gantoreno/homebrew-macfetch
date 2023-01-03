aa673c3ad15a6700d28305bb18a8887a989d6501
Gabriel Moreno <gabriel.moreno@zemoga.com>
2023-01-03 16:08:24 -0500

    Update sha256 hash

--- macfetch.rb
+++ macfetch.rb
@@ -2,7 +2,7 @@ class Macfetch < Formula
   desc "🍎 A macOS Neofetch alternative written in C++"
   homepage "https://github.com/gantoreno/macfetch"
   url "https://github.com/gantoreno/macfetch/releases/download/v1.0.1/macfetch-v1.0.1.tar.gz"
-  sha256 "bd8531b143e34162d2844cc2522760aaa265c79362cdd1f328255b0791ee85d9"
+  sha256 "2655e7aa896e604b55893541788cab175fb54fbea03b637f718c1b12905a0294"
   license "MIT"
 
   def install
