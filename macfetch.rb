14569ea5728888d8eb11f8d8adbfcc1a73ff20ea
Gabriel Moreno <gabriel.moreno@zemoga.com>
2023-02-19 10:19:45 -0500

    Fix broken link

--- macfetch.rb
+++ macfetch.rb
@@ -1,7 +1,7 @@
 class Macfetch < Formula
   desc "🍎 A macOS Neofetch alternative written in C++"
   homepage "https://github.com/gantoreno/macfetch"
-  url "https://github.com/gantoreno/macfetch/releases/download/v1.1.0/macfetch-v1.1.1.tar.gz"
+  url "https://github.com/gantoreno/macfetch/releases/download/v1.1.1/macfetch-v1.1.1.tar.gz"
   sha256 "31c6ea28d2eb637562909114ff27e9268ce7124c0d1eb6e6234152fd8d32362f"
   license "MIT"
 
